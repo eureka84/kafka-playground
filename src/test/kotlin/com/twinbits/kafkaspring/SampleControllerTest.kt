f939072beee47da980ebb48d0286045eb830d42e
Angelo Sciarra <angelosciarra@ymail.com>
2022-09-17 22:02:42 +0200

    added auto creation for topics

--- src/test/kotlin/com/twinbits/kafkaspring/SampleControllerTest.kt
+++ src/test/kotlin/com/twinbits/kafkaspring/SampleControllerTest.kt
@@ -3,6 +3,7 @@ package com.twinbits.kafkaspring
 import org.assertj.core.api.Assertions.assertThat
 import org.awaitility.Awaitility.await
 import org.junit.jupiter.api.Test
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc
 import org.springframework.boot.test.context.SpringBootTest
@@ -20,6 +21,8 @@ import java.time.Duration
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebMvc
 class SampleControllerTest {
+
+    private val logger = LoggerFactory.getLogger(SampleControllerTest::class.java)
 
     @LocalServerPort
     var localServerPort: Int = 0
@@ -45,11 +48,19 @@ class SampleControllerTest {
     }
 
     companion object {
+
+        private val LOGGER = LoggerFactory.getLogger(SampleControllerTest::class.java)
+
         @JvmStatic
         @DynamicPropertySource
         fun configureMysql(registry: DynamicPropertyRegistry) {
             val dbPort = System.getProperty("db_1.tcp.3306")
-            registry.add("spring.datasource.url"){"jdbc:mysql://localhost:$dbPort/db"}
+            registry.add("spring.datasource.url") { "jdbc:mysql://localhost:$dbPort/db" }
+
+            val brokerPort = System.getProperty("broker.tcp.9092")
+            LOGGER.info("Dynamic kafka port: $brokerPort")
+            registry.add("spring.kafka.bootstrap-servers") { "localhost:$brokerPort" }
+            registry.add("spring.kafka.consumer.bootstrap-servers") { "localhost:$brokerPort" }
         }
 
     }
